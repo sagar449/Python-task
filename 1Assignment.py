bd83189748334449f9f91e0d0ee579165c47205a
sagar449 <68236853+sagar449@users.noreply.github.com>
2020-07-14 23:16:52 +0530

    Add files via upload

---
+++ 1Assignment.py
@@ -0,0 +1,20 @@
+#print("hello")
+
+print("\nContact Form\n")
+name = raw_input("Enter Name:-")
+age = int(input("Enter age:-"))
+course = raw_input("Enter Course:-")
+contact = int(input("Enter contact"))
+conf = raw_input("Above info is True or Not")
+
+print("\nResult is")
+
+print("We got entered name:-", name)
+print("We got entered age:-", age)
+print("We got entered course:-", course)
+print("We got entered contact:-", contact)
+print("Entered information is", conf)
+
+
+
+
