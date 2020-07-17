879dd862fceed07a7249985e174ce2ff2b722dab
sagar449 <68236853+sagar449@users.noreply.github.com>
2020-07-17 22:25:14 +0530

    Add files via upload

---
+++ 6th july batch/assignment3.py
@@ -0,0 +1,10 @@
+uname = "aman64039"
+pasword = 147852
+
+username = raw_input("Please enter the username")
+password = int(raw_input("Please enter the password"))
+
+if uname == username and pasword == password:
+    print("Hello", username , "Welcome here")
+else:
+    print("Please enter valid username and password")
